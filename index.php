a8b4dc46922b6c451ef32e9b1759a483a9aa8d7c
Adrian S <4drian.s.m@gmail.com>
2018-09-28 05:37:48 -0500

    index y login - bootstrap

--- index.php
+++ index.php
@@ -2,74 +2,103 @@
 <html lang="es">
 <head>
 	<title>Agora Virtual</title>
-	<meta charset="utf-8">
-  <link rel="stylesheet" type="text/css" href="css/reset.css">
-	<link rel="stylesheet" type="text/css" href="css/responsive.css">
-	<script type="text/javascript" src="js/jquery.js"></script>
-	<script type="text/javascript" src="js/main.js"></script>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="vendor/main.css" rel="stylesheet">
 </head>
 <body>
-  <header>
-	  <div class="wrapper">
-	  	<a href="index.php"><img src="img/AgoraVirtual.png" class="logo"/></a>
-	  	<a href="#" class="hamburger"></a>
-	  	<nav>
-	  	  <ul>
-	  	    <li><a href="index.php?opcion=0">Inicio</a></li> 
-	  	    <li><a href="index.php?opcion=1">Bebidas</a></li>
-	  	    <li><a href="index.php?opcion=2">Comida</a></li>
-	  	    <li><a href="index.php?opcion=3">Dulces</a></li>
-	  	    <li><a href="index.php?opcion=4">Cigarros</a></li>
-	  	    <li><a href="index.php?opcion=5">Postres</a></li>
-	  	  </ul>
-	  		<a href="usr.php" class="login_btn">Yo Vendo</a>
-	  	</nav>
-	  </div>
-	</header>
-
-	<section class="listings">
-		<div class="wrapper">
-			<ul class="properties_list">
+  <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="index.php">
+        <img src="img/AgoraVirtual.png">
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="index.php?opcion=0">Inicio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="index.php?opcion=1">Bebidas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php?opcion=2">Comida</a>
+          </li>    
+          <li class="nav-item">
+            <a class="nav-link" href="index.php?opcion=3">Dulces</a>
+          </li>    
+          <li class="nav-item">
+            <a class="nav-link" href="index.php?opcion=4">Cigarros</a>
+          </li>    
+          <li class="nav-item">
+            <a class="nav-link" href="index.php?opcion=5">Postres</a>
+          </li>        
+        </ul>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a href="usr.php" class="btn btn-outline-light btn-sm" role="button">Yo vendo</a>
+          </li>
+        </ul>
+      </div>
+    </div>  
+  </nav>
+  
+  <div class="container" style="margin-top:80px;">
+    <div class="row d-flex justify-content-around">
 
       <?php
       include("connect.php");      
-
-        $option = $_GET['opcion'];
-        
-        if($option == 0){
-          $query = mysqli_query($link, "SELECT * FROM productos INNER JOIN vendedores ON productos.id_vendedor = vendedores.id_vendedor");
-
-            while ($row = mysqli_fetch_assoc($query)) {
-            echo '<li><img src="img/food_3.jpg" class="property_img"/>
-                    <span class="price">$'.$row['precio'].'</span>
-                    <div class="property_details">
-                    <h1>'.$row['nombre'].'</h1>
-                    <p>'.$row['descripcion'].'</p>
-                    <h3>'.$row['nombre_vendedor'].'</h3>
-                    </div>
-                  </li>';
-            }
-        }else{
-
-          $query = mysqli_query($link, "SELECT * FROM productos INNER JOIN vendedores ON  productos.id_vendedor = vendedores.id_vendedor where id_categoria = $option");
-
-            while ($row = mysqli_fetch_assoc($query)) {
-            echo '<li><img src="img/food_3.jpg" class="property_img"/>
-                    <span class="price">$'.$row['precio'].'</span>
-                    <div class="property_details">
-                    <h1>'.$row['nombre'].'</h1>
-                    <p>'.$row['descripcion'].'</p>
-                    <h3>'.$row['nombre_vendedor'].'</h3>
-                    </div>
-                  </li>';
-            }
+      $option = $_GET['opcion'];
+      
+      if($option == 0){
+        $query = mysqli_query($link, "SELECT * FROM productos INNER JOIN vendedores ON productos.id_vendedor = vendedores.id_vendedor");
+        while ($row = mysqli_fetch_assoc($query)) {
+            echo  '<div class="col-">
+                <div class="card">
+                <img class="card-img-top" src="img/food_1.jpg" alt="Card image cap">
+                <div class="card-img-overlay">
+                  <h5 class="card-title">'.$row['precio'].'</h5>
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">'.$row['nombre'].'</h5>            
+                  <p class="card-text"><small>'.$row['descripcion'].'</small></p>
+                  <p class="card-text">'.$row['nombre_vendedor'].'</p>
+                </div>
+              </div></div>';
         }
+      }else{
+        $query = mysqli_query($link, "SELECT * FROM productos INNER JOIN vendedores ON  productos.id_vendedor = vendedores.id_vendedor where id_categoria = $option");
+          while ($row = mysqli_fetch_assoc($query)) {
+            echo  '<div class="col-">
+                <div class="card">
+                <img class="card-img-top" src="img/food_1.jpg" alt="Card image cap">
+                <div class="card-img-overlay">
+                  <h5 class="card-title">'.$row['precio'].'</h5>
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">'.$row['nombre'].'</h5>            
+                  <p class="card-text">'.$row['descripcion'].'</p>
+                  <p class="card-text"><small class="text-muted">'.$row['nombre_vendedor'].'</small></p>
+                </div>
+              </div></div>';
+          }
+      }
       ?>
-			</ul>
-		</div>
-	</section>
+    </div>
+  </div>
+  <br/>
+  <br/>
+  <footer class="py-3 bg-dark fixed-bottom">
+    <div class="container">
+      <p class="m-0 text-center text-white"></p>
+    </div>
+  </footer>
+  
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-	<footer>
-	</footer>
 </body>
 </html>
--- login.php
+++ login.php
@@ -31,44 +31,74 @@
 <html lang="es">
 	<head>
 		<title>Agora Virtual</title>
-		<meta charset="utf-8">
-		<link rel="stylesheet" type="text/css" href="css/reset.css">
-		<link rel="stylesheet" type="text/css" href="css/responsive.css">
-		<script type="text/javascript" src="js/jquery.js"></script>
-		<script type="text/javascript" src="js/main.js"></script>
+	  <meta charset="utf-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	  <link href="vendor/main.css" rel="stylesheet">
 	</head>
 	<body>
-	  <header>
-	  	<div class="wrapper">
-	  		<a href="#"><img src="img/AgoraVirtual.png" class="logo"/></a>
-	  		<a href="#" class="hamburger"></a>
-	  		<nav>
-	  		  <ul>
-	  		    <li><a href="index.php?opcion=0">Inicio</a></li> 
-	  		    <li><a href="index.php?opcion=1">Bebidas</a></li>
-	  		    <li><a href="index.php?opcion=2">Comida</a></li>
-	  		    <li><a href="index.php?opcion=3">Dulces</a></li>
-	  		    <li><a href="index.php?opcion=4">Postres</a></li>
-	  		    <li><a href="index.php?opcion=5">Cigarros</a></li>
-	  		  </ul>
-	  			<a href="usr.php" class="login_btn">Yo Vendo</a>
-	  		</nav>
-	  	</div>
-	  </header>
-	  <section class="listings">
-			<h2>Enter Username and Password</h2> 
-      	<div class="container">      
-      	  <form class="form-signin" role="form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method = "post">      	              
-      	     <label>mail  :</label><input type="text" class="form-control" name="username" placeholder="admin@mail.com" required autofocus><br/>
-      	     <label>Password  :</label><input type="password" class="form-control" name="password" placeholder="admin" required><br/>
-      	     <p><button class="btn btn-lg btn-primary btn-block" type="submit" name="login">Login</button></p>
-      	  </form>
-      	  <button><a href = "logout.php">Clean Session.</a></button>
-      	</div>
-	  </section>
-		<footer>
-			<p class="copyrights wrapper"></p>
-		</footer>
+  	<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+  	  <div class="container">
+  	    <a class="navbar-brand" href="index.php">
+  	      <img src="img/AgoraVirtual.png">
+  	    </a>
+  	    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+  	      <span class="navbar-toggler-icon"></span>
+  	    </button>
+  	    <div class="collapse navbar-collapse" id="navbarResponsive">
+  	      <ul class="navbar-nav ml-auto">
+  	        <li class="nav-item">
+  	          <a class="nav-link js-scroll-trigger" href="index.php?opcion=0">Inicio</a>
+  	        </li>
+  	        <li class="nav-item">
+  	          <a class="nav-link js-scroll-trigger" href="index.php?opcion=1">Bebidas</a>
+  	        </li>
+  	        <li class="nav-item">
+  	          <a class="nav-link" href="index.php?opcion=2">Comida</a>
+  	        </li>    
+  	        <li class="nav-item">
+  	          <a class="nav-link" href="index.php?opcion=3">Dulces</a>
+  	        </li>    
+  	        <li class="nav-item">
+  	          <a class="nav-link" href="index.php?opcion=4">Cigarros</a>
+  	        </li>    
+  	        <li class="nav-item">
+  	          <a class="nav-link" href="index.php?opcion=5">Postres</a>
+  	        </li>        
+  	      </ul>
+  	      <ul class="navbar-nav">
+  	        <li class="nav-item">
+  	          <a href="usr.php" class="btn btn-outline-light btn-sm" role="button">Yo vendo</a>
+  	        </li>
+  	      </ul>
+  	    </div>
+  	  </div>  
+  	</nav>		
+		<div class="container">
+	 		<div id="cont_admin">
+			 	<form class="form-signin" role="form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method = "post">      	              
+					 <label>mail  :</label>
+					 <input type="text" class="form-control" name="username" placeholder="admin@mail.com" required autofocus><br/>
+					 <label>Password  :</label>
+					 <input type="password" class="form-control" name="password" placeholder="admin" required><br/>
+					 <br/>
+					 <br/>
+					 <div class="text-center">
+							<button type="submit" name="login" class="btn btn-primary col-5 col-sm-5 col-md-5">Iniciar sesión</button>
+							<a href="#"><button class="btn btn-primary col-5 col-sm-5 col-md-5">Registrarse</button></a>
+						</div>
+				</form>
+			</div>
+		</div>
+		
+		<footer class="py-3 bg-dark fixed-bottom">
+    	<div class="container">
+    	  <p class="m-0 text-center text-white"></p>
+  	  </div>
+  	</footer>
+  
+  	<script src="vendor/jquery/jquery.min.js"></script>
+  	<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 	</body>
 </html>
 
